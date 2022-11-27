1aac9c0a812a178ac16b082dbeca2ea0b6917f20
nnen9 <gigrys.drop@gmail.com>
2022-11-27 16:29:45 +0300

    DataBase.v.001 complited

---
+++ WindowsFormsApp1/DataBaseLib/Commands/Table1Commands.cs
@@ -0,0 +1,50 @@
+﻿using DataBaseLib.Access;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBaseLib.Commands
+{
+    internal class Table1Commands : ITableEditorCommand
+    {
+        // ЕСЛИ нужна другая БД, просто меняем AccessDataBaseController
+        // на нужную, например SQLiteDataBaseController
+        // в следующей строке
+        private AccessDataBaseController controller = new AccessDataBaseController();
+
+        public void Select(string[] args)
+        {
+
+        }
+
+        public void Insert(string[] args)
+        {            
+            string query = $"INSERT INTO [R1] " +
+                    $"([ФИО], [Специальность], [Возраст]) " +
+                    $"VALUES ('{args[0]}', '{args[1]}', {args[2]})";
+            controller.ExecuteCommand(query);
+        }
+
+        public void Update(string[] args)
+        {
+            // дома
+
+            /*
+             string query = $"UPDATE [Категории] SET [Категория товара] = '{type}', " +
+                    $"[Скидка %] = {discount} " +
+                    $"WHERE [ID] = {id}"; 
+             */
+        }
+
+        public void Delete(string[] args)
+        {
+            string query = $"DELETE FROM [Основная] " +
+                    $"WHERE [Код] = {args[0]}";
+            controller.ExecuteCommand(query);
+        }
+
+
+    }
+}
